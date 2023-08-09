8997076d2ae645f776e54241ac2a81913a4aced7
Navin-Gelot <navin.gelot@valensdatalabs.com>
2023-08-09 15:25:47 +0530

    More changes

--- eureka-server/src/main/java/com/client/serviceregistrationanddiscoveryservice/rest/UserApplication.java
+++ eureka-server/src/main/java/com/client/serviceregistrationanddiscoveryservice/rest/UserApplication.java
@@ -1,5 +1,6 @@
 package com.client.serviceregistrationanddiscoveryservice.rest;
 
+import com.client.core.dto.Employee;
 import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,17 +14,16 @@ public class UserApplication {
     private final WebClient.Builder loadBalancedWebClientBuilder;
     private final ReactorLoadBalancerExchangeFilterFunction lbFunction;
 
-    public UserApplication(WebClient.Builder webClientBuilder,
-                           ReactorLoadBalancerExchangeFilterFunction lbFunction) {
+    public UserApplication(WebClient.Builder webClientBuilder, ReactorLoadBalancerExchangeFilterFunction lbFunction) {
         this.loadBalancedWebClientBuilder = webClientBuilder;
         this.lbFunction = lbFunction;
     }
 
-    @RequestMapping("/hi")
-    public Mono<String> hi(@RequestParam(value = "name", defaultValue = "Mary") String name) {
-        return loadBalancedWebClientBuilder.build().get().uri("http://client-application-instance/greeting")
-                .retrieve().bodyToMono(String.class)
-                .map(greeting -> String.format("%s, %s!", greeting, name));
+    @RequestMapping(value = "/employee", produces = "application/json" )
+    public Mono<Employee> hi(@RequestParam(value = "name", defaultValue = "Mary") String name) {
+        return loadBalancedWebClientBuilder.build().get().uri("http://client-application-instance/employee")
+                .retrieve().bodyToMono(Employee.class)
+                .map(employee -> employee);
     }
 
     @RequestMapping("/hello")
